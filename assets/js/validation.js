9a53ffd4984a00763104bebf6c17b90f771987f8
Michael Mancini <mich.mancini06@gmail.com>
2022-04-12 16:28:04 +0200

    added js for form

---
+++ assets/js/validation.js
@@ -0,0 +1,50 @@
+/* Validation of form */
+const validateForm = () => {
+  if (document.dati.inputNome.value == "") {
+    alert("Il campo Nome è vuoto");
+    return false;
+  }
+  if (document.dati.inputCognome.value == "") {
+    alert("Il campo Cognome è vuoto");
+    return false;
+  }
+  if (document.dati.inputEmail.value == "") {
+    alert("Il campo Email è vuoto");
+    return false;
+  }
+  if (document.dati.inputPassword.value == "") {
+    alert("Il campo Password è vuoto");
+    return false;
+  }
+  if (document.dati.inputIndirizzo.value == "") {
+    alert("Il campo Indirizzo è vuoto");
+    return false;
+  }
+  if (document.dati.inputCitta.value == "") {
+    alert("Il campo Città è vuoto");
+    return false;
+  }
+  if (document.dati.CAP.value == "") {
+    alert("Il campo CAP è vuoto");
+    return false;
+  }
+  if (!document.dati.inputCheck.checked) {
+    alert("Per registrarti devi acconsentire all'invio dei dati");
+    return false;
+  }
+  alert("Dati inseriti correttamente");
+  return true;
+};
+
+/* Validation of Login Form */
+const validateLogin = () => {
+  if (!document.dati.inputEmail.value) {
+    alert("Il campo Email è vuoto");
+    return false;
+  }
+  if (!document.dati.inputPassword.value) {
+    alert("Il campo Password è vuoto");
+    return false;
+  }
+  return true;
+};
