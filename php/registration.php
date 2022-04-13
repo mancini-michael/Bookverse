4957d177a06b76a56e34ef60af4333f104e63fdb
Michael Mancini <mich.mancini06@gmail.com>
2022-04-13 17:19:50 +0200

    fixed registration php

--- php/registration.php
+++ php/registration.php
@@ -22,7 +22,7 @@ $met_enc = 'aes256';
 $iv = 'mD1g7i9fD56_hf12';
 $pass_enc = openssl_encrypt($password, $met_enc, $key_enc, 0, $iv);
 $query2 = 'INSERT INTO users VALUES ($1,$2,$3,$4,$5,$6,$7)';
-$result = pg_query_params($dbconn, $query2, array($nome, $cognome, $email, $pass_enc, $indirizzo, $citta, $cap));
+$result = pg_query_params($connection, $query2, array($nome, $cognome, $email, $pass_enc, $indirizzo, $citta, $cap));
 
 if (!$result) {
     die("ERRORE: inserimento nel database non riuscito");
