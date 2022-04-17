c51f615c45bb17dd26fe2a9e20071f0780c029db
Michael Mancini <mich.mancini06@gmail.com>
2022-04-17 11:23:48 +0200

    finished catalog

--- components/item.php
+++ components/item.php
@@ -1,15 +1,17 @@
-<div class="col-md-3 mx-auto">
-    <div class="container mx-auto my-3">
-        <img src=<?php echo $img; ?> alt=<?php echo $nome; ?> class="img-fluid m-1">
-        <h5 class="m-1"> <?php echo $nome; ?> </h5>
-        <span class="m-1"> <?php echo $autore; ?> </span>
-        <br />
-        <span class="m-1">isbn: <?php echo $isbn; ?> </span>
-        <br />
-        <span class="m-1">pubblicato: <?php echo $data_rilascio; ?> </span>
-        <br />
-        <span class="m-1">prezzo: <?php echo $prezzo; ?> </span>
-        <br />
-        <a href="#" class="btn btn-primary">Acquista</a>
+<div class="col my-3">
+    <div class="card mx-auto">
+        <img src=<?php echo $img; ?> class="img-fluid" alt=<?php echo $nome; ?>>
+        <div class="card-body">
+            <h4 class="card-title text-black"> <?php echo $nome; ?> </h4>
+            <h5 class="card-text text-black m-0"> <?php echo $autore; ?> </h5>
+            <br />
+            <span class="card-text text-black"> <?php echo $data_rilascio; ?> </span>
+            <br />
+            <span class="card-text text-black">isbn: <?php echo $isbn; ?> </span>
+            <br />
+            <span class="card-text text-black bold">prezzo: <?php echo $prezzo; ?> </span>
+            <br />
+            <a href="checkout.php?isbn=<?php echo $isbn; ?>" class="btn btn-info my-2">Acquista</a>
+        </div>
     </div>
 </div>
