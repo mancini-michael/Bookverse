f0f06670e8989aaf2c78ba362a0881716ff5ae8c
Michael Mancini <mich.mancini06@gmail.com>
2022-04-19 16:38:10 +0200

    remoeved useless echo

--- php/config.php
+++ php/config.php
@@ -11,8 +11,6 @@ define("password", $_ENV["DB_PASSWORD"]);
 $connection_string = "host=" . host . " port=" . port . " dbname=" . dbname . " user=" . user . " password=" . password;
 $connection = pg_connect($connection_string);
 
-echo $connection_string;
-
 if (!$connection) {
     echo $connection;
 }
