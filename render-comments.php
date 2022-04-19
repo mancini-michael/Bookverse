7d9ad04d32375afcad4f24911c84d49848cbd3c4
loregi01 <gizzilorenzo@virgilio.it>
2022-04-19 14:34:59 +0200

    usersComments

---
+++ render-comments.php
@@ -0,0 +1,31 @@
+<?php
+    require_once("php/config.php");
+    if ($connection) {
+        session_start();
+        $email = $_SESSION["email"];
+        
+        $q = "SELECT * FROM comments_users WHERE email=$1";
+        $result = pg_query_params($connection, $q, array($email));
+
+        $commenti = pg_fetch_all($result);
+
+        if ($commenti == null) {
+            echo "<div align=\"center\" style=\"margin-top:250px; background-color:black; padding-top:50px; padding-bottom:50px\">" .
+                    "<h1 style=\"color:white\"> Non hai inserito commenti </h1>" .
+                 "</div>";
+        }
+        else {
+            $arrLen = count($commenti);
+        
+            for ($i = 0; $i < $arrLen; $i++) {
+                $nome_libro = $commenti[$i]['nome_libro'];
+                $descrizione = $commenti[$i]['descrizione'];
+                echo "<h1>Libro: </h1>" .  "$nome_libro" . " <h1>Descrizione: </h1> " .  "$descrizione<hr />";
+            }
+        }
+
+        pg_close($connection);
+    } else {
+        echo "Internal Server Error";
+    }
+?>
