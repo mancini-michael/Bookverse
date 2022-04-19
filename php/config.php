08df30caaab1e0d18afd620c53e96f24c8d38282
Michael Mancini <mich.mancini06@gmail.com>
2022-04-19 16:36:16 +0200

    added environment variables

--- php/config.php
+++ php/config.php
@@ -1,13 +1,17 @@
 <?php
 
+require_once("environment.php");
+
 define("host", "localhost");
 define("port", "5432");
-define("dbname", "...");
+define("dbname", $_ENV["DB_NAME"]);
 define("user", "postgres");
-define("password", "...");
+define("password", $_ENV["DB_PASSWORD"]);
 
 $connection_string = "host=" . host . " port=" . port . " dbname=" . dbname . " user=" . user . " password=" . password;
 $connection = pg_connect($connection_string);
+
+echo $connection_string;
 
 if (!$connection) {
     echo $connection;
