7c35e8dc5df92bef1cb73f2c67fc4560e2903a11
loregi01 <gizzilorenzo@virgilio.it>
2022-04-20 19:49:27 +0200

    addPurchasesList

--- php/acquisti.php
+++ php/acquisti.php
@@ -1,3 +1,31 @@
 <?php
-    echo "I tuoi acquisti";
+    require_once("config.php");
+    if ($connection) {
+        session_start();
+        $email = $_SESSION["email"];
+        $q = "SELECT * FROM user_purchases WHERE email=$1";
+        $result = pg_query_params($connection, $q, array($email));
+
+        $acquisti = pg_fetch_all($result);
+
+        if ($acquisti == null) {
+            echo "<div align=\"center\" style=\"margin-top:250px; background-color:black; padding-top:50px; padding-bottom:50px\">" .
+                    "<h1 style=\"color:white\"> Non hai effettuato acquisti </h1>" .
+                 "</div>";
+        }
+        else {
+            $arrLen = count($acquisti);
+        
+            for ($i = 0; $i < $arrLen; $i++) {
+                $isbn = $acquisti[$i]['isbn'];
+                $prezzo = $acquisti[$i]['prezzo'];
+                echo "<h1>isbn: </h1>" .  "$isbn" . " <h1>prezzo: </h1> " .  "$prezzo<hr />";
+            }
+        }
+
+        pg_close($connection);
+    } else {
+        echo "Internal Server Error";
+    }
+
 ?>
