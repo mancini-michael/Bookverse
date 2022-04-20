4cf1cd27b9d2714a163790e72cd130d07c21c733
loregi01 <gizzilorenzo@virgilio.it>
2022-04-20 19:13:50 +0200

    newConfig

--- php/config.php
+++ php/config.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once ("environment.php");
+
 define("host", "localhost");
 define("port", "5432");
-define("dbname", "...");
+define("dbname", $_ENV["DB_NAME"]);
 define("user", "postgres");
-define("password", "...");
+define("password", $_ENV["DB_PASSWORD"]);
 
 $connection_string = "host=" . host . " port=" . port . " dbname=" . dbname . " user=" . user . " password=" . password;
 $connection = pg_connect($connection_string);
@@ -12,3 +14,5 @@ $connection = pg_connect($connection_string);
 if (!$connection) {
     echo $connection;
 }
+
+?>
