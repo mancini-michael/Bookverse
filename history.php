d1ffe550b7ac1185225c9081f012cb470602021b
loregi01 <gizzilorenzo@virgilio.it>
2022-04-23 09:41:49 +0200

    fixedHomePageButton

--- history.php
+++ history.php
@@ -23,7 +23,7 @@
         <div class="container row mx-auto my-auto">
             <?php include("php/history.php"); ?>
         </div>
-        <a href="./welcome.php" class="btn btn-primary mb-3">Torna alla homepage</a>
+        <a href="./welcome.php" class="btn btn-primary mb-3" style="margin-top:10px">Torna alla homepage</a>
     </div>
 
     <?php include("components/footer.php"); ?>
