5d05990d1a67519ea34ec661e576a8c6b107e7b9
Michael Mancini <mich.mancini06@gmail.com>
2022-05-08 14:41:36 +0200

    final fix

--- welcome.php
+++ welcome.php
@@ -21,32 +21,38 @@
 
     <div class="container-fluid user row justify-content-around m-auto pt-5">
 
-    <?php
+        <?php
         if (isset($_GET["changePassword"])) {
-            echo "<div class=\"container alert alert-success text-center mt-5\">
-                    Password modificata con successo
-                  </div>";
+            echo "
+                <div class=\"container-md1\">
+                    <div class=\"alert alert-success text-center mt-5\">
+                        Password modificata con successo
+                    </div>
+                </div>";
         }
 
-    ?>  
+        ?>
 
-    <?php
+        <?php
         if (isset($_GET["acquisto"])) {
-            echo "<div class=\"container alert alert-success text-center mt-5\">
+            echo "
+            <div class=\"container-md\">
+                <div class=\"alert alert-success text-center mt-5\">
                     Acquisto andato a buon fine
-                  </div>";
+                </div>
+            </div>";
         }
 
-    ?>  
+        ?>
 
-        <div class="col-lg-3 my-lg-auto mt-5">
+        <div class="col-lg-3 my-lg-auto m-auto mb-5">
             <div class="user-icon d-flex justify-content-center align-items-center mx-auto">
                 <h1 class="m-0"><?php echo $_SESSION["nome"][0]; ?></h1>
             </div>
         </div>
 
-        <div class="col-lg-6 text-center text-white my-auto">
-            <div class="user-info rounded-3 m-5 p-3">
+        <div class="col-lg-6 text-center text-white m-auto">
+            <div class="container-md user-info rounded-3 m-auto p-5">
                 <div class="d-flex justify-content-center align-items-center flex-column h-100 mt-3">
                     <h1>Informazioni personali</h1>
                     <span> <b>nome:</b> <?php echo $_SESSION["nome"]; ?></span>
